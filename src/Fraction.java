6f06e786c5413b65e4a49bbb8c42eb0bb85fa4a9
Marion_Chevallier <marion.chevallier134@gmail.com>
2020-09-23 18:02:42 +0200

    Main sans assert

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,3 +1,30 @@
 public class Fraction {
 
+    private float numerateur;
+    private float denominateur;
+    private float fraction;
+
+    public Fraction(float numerateur,float denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+        this.fraction = this.numerateur/this.denominateur;
+    }
+
+    public float getFraction() {
+        return fraction;
+    }
+
+    public float getDenominateur() {
+        return denominateur;
+    }
+
+    public float getNumerateur() {
+        return numerateur;
+    }
+
+    public String toString() {
+        return this.numerateur+"/"+this.denominateur+" = "+this.fraction;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+        Fraction F = new Fraction(4,0);
+        
+        System.out.println(F.toString());
+
         System.out.println("Ca marche");
     }
 
